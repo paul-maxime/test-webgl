299739af520bdcce9fe336bc0339de0e742f9341
Paul-Maxime <paulmaxime@openmailbox.org>
2016-08-02 13:03:48 +0100

    Remove unused functions in Buffer

--- src/buffer.js
+++ src/buffer.js
@@ -15,31 +15,4 @@ class Buffer {
 		this.bind();
 		this.gl.bufferData(this.gl.ARRAY_BUFFER, data, this.gl.STATIC_DRAW);
 	}
-	setArray(data) {
-		this.setFloat32Array(new Float32Array(data));
-	}
-	setDataFromSpriteVertices(width, height) {
-		this.setArray([
-			width, height, 0.0,
-			0.0, height, 0.0,
-			width, 0.0, 0.0,
-			0.0, 0.0, 0.0
-		]);
-	}
-	setDataFromTextureCoordinates(startX, startY, endX, endY) {
-		this.setArray([
-			endX, endY,
-			startX, endY,
-			endX, startY,
-			startX, startY
-		]);
-	}
-	setDataFromColor(r, g, b, a) {
-		this.setArray([
-			r, g, b, a,
-			r, g, b, a,
-			r, g, b, a,
-			r, g, b, a
-		]);
-	}
 }
