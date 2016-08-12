90053d401a4bcbf88fa1e0a59e086b0f5d7127d5
Paul-Maxime <paulmaxime@openmailbox.org>
2016-08-12 22:16:16 +0200

    Add gulp, a gitignore and move the test to a folder

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var source = require('vinyl-source-stream')
+var streamify = require('gulp-streamify')
+var browserify = require('browserify')
+var babelify = require('babelify')
+var uglify = require('gulp-uglify')
+var rename = require('gulp-rename')
+var gulp = require('gulp')
+var del = require('del')
+
+gulp.task('clean', function() {
+	return del(['build']);
+});
+
+gulp.task('default', ['clean'], function() {
+	return browserify('./src/index.js')
+		.transform('babelify')
+		.bundle()
+		.pipe(source('index.js'))
+		//.pipe(streamify(uglify()))
+		.pipe(rename('yaje.min.js'))
+		.pipe(gulp.dest('build'))
+});
