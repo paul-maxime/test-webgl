fe27c21fd1f12835bdd0e683dc08a683b84987a3
Paul-Maxime <paulmaxime@openmailbox.org>
2016-09-24 23:03:15 +0200

    Add a basic music player

---
+++ src/musicplayer.js
@@ -0,0 +1,37 @@
+'use strict';
+
+export default class MusicPlayer {
+	constructor() {
+		this.musics = {};
+		this.currentMusic = null;
+	}
+	register(name, path) {
+		this.musics[name] = new Audio(path);
+	}
+	play(name) {
+		var music = this.musics[name];
+		if (this.currentMusic != music) {
+			if (this.currentMusic != null) {
+				this.currentMusic.pause();
+				this.currentMusic.currentTime = 0;
+			}
+			this.currentMusic = music;
+			if (music != null) {
+				music.play();
+			}
+		}
+	}
+	pause() {
+		if (this.currentMusic != null) {
+			this.currentMusic.pause();
+		}
+	}
+	resume() {
+		if (this.currentMusic != null) {
+			this.currentMusic.play();
+		}
+	}
+	stop() {
+		this.play(null);
+	}
+}
