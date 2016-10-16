e6c0c0f5b3d103c6017dfbc85821e53b93d585ac
Paul-Maxime <paulmaxime@openmailbox.org>
2016-10-16 21:54:20 +0200

    Fix the missing this/other in BoxCollider

--- src/boxcollider.js
+++ src/boxcollider.js
@@ -9,12 +9,12 @@ export default class BoxCollider {
 		this.height = height;
 	}
 	intersectWith(other) {
-		let selfX1 = this.sprite.position[0] + x;
-		let selfY1 = this.sprite.position[1] + y;
+		let selfX1 = this.sprite.position[0] + this.x;
+		let selfY1 = this.sprite.position[1] + this.y;
 		let selfX2 = selfX1 + this.width;
 		let selfY2 = selfY1 + this.height;
-		let otherX1 = other.sprite.position[0] + x;
-		let otherY1 = other.sprite.position[1] + y;
+		let otherX1 = other.sprite.position[0] + other.x;
+		let otherY1 = other.sprite.position[1] + other.y;
 		let otherX2 = otherX1 + other.width;
 		let otherY2 = otherY1 + other.height;
 		return !(
