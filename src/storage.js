df8d5bae75fd9f4db9311752e8231edf8c4cd0fa
Paul-Maxime <github@paulmaxime.fr>
2017-01-26 23:44:05 +0100

    Documents the Storage class

--- src/storage.js
+++ src/storage.js
@@ -1,10 +1,27 @@
 "use strict";
 
+/**
+ * This class is used to store data in the browser's LocalStorage.
+ *
+ * Instantiate it, store information in the Storage object then call the load() method.
+ *
+ * @class
+ * @memberof Yaje
+ */
 export default class Storage {
+	/**
+	 * Loads the object specified by its key from the LocalStorage.
+	 *
+	 * @param {string} keyname - Object key, should be unique to your application.
+	 */
 	constructor(keyname) {
 		this.keyname = keyname;
 		this.load();
 	}
+	/**
+	 * Reloads the object from the LocalStorage.
+	 * You do not have to call this method, the constructor already calls it.
+	 */
 	load() {
 		var json = localStorage.getItem(this.keyname);
 		if (json) {
@@ -14,6 +31,9 @@ export default class Storage {
 			}
 		}
 	}
+	/**
+	 * Saves the object to the LocalStorage.
+	 */
 	save() {
 		var json = JSON.stringify(this);
 		localStorage.setItem(this.keyname, json);
