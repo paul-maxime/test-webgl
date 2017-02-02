cb8a8e908595f061b5e70beba81eed89f4aa31ed
Paul-Maxime <github@paulmaxime.fr>
2017-02-02 23:58:54 +0100

    Improve the Clock documentation

--- src/clock.js
+++ src/clock.js
@@ -1,25 +1,43 @@
 "use strict";
 
+/**
+ * A clock that can be used to measure time.
+ *
+ * @class
+ * @memberof Yaje
+ */
 export default class Clock {
-/**
- * @constructor
- * Create an instance of Clock object.
- *
- * @example
- * clock = new Yaje.Clock();
- */
+	/**
+	 * Creates an instance of Clock object.
+	 *
+	 * @example
+	 * var clock = new Yaje.Clock();
+	 */
 	constructor() {
-		this.lastUpdate = window.performance.now();
+		/**
+		 * Elapsed time between two calls to update(), in seconds.
+		 * @member {number}
+		 */
 		this.deltaTime = 0;
+
+		/**
+		 * Current total time in milliseconds.
+		 * @private
+		 * @member {number}
+		 */
+		this.lastUpdate = window.performance.now();
 	}
-/**
- * Calculate the new deltaTime.
- *
- * @returns {number} Returns the new deltaTime.
- *
- * @example
- * clock.update();
- */
+	/**
+	 * Calculates the elapsed time since the last call to update.
+	 *
+	 * @returns {number} Returns the elapsed time, in seconds.
+	 *
+	 * @example
+	 * // displays the elapsed time in seconds.
+	 * console.log(clock.update());
+	 * // displays the same value, but does not update.
+	 * console.log(clock.deltaTime);
+	 */
 	update() {
 		var now = window.performance.now();
 		var delta = now - this.lastUpdate;
